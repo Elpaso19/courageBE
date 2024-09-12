04c2cd4b28ae5721f73e7c0b00ad76a7cce30548
Elpaso19 <georgegodgift7@gmail.com>
2024-09-12 19:05:30 +0100

    tt

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors({ origin: 'http://localhost:5173', credentials: true })); // Adjust CORS origin as needed
+app.use(cors({ origin: 'https://courage-quiz.onrender.com', credentials: true })); // Adjust CORS origin as needed
 
 // Passport.js middleware
 require('./config/passport'); // Initialize Passport configuration
